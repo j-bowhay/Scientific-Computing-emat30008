800eb6f812770c7db6482fc1e4439ace6bf64511
Jake Bowhay <jb9.bowhay@gmail.com>
2023-02-03 12:52:19 +0000

    error plot

--- src/scicomp/integrate.py
+++ src/scicomp/integrate.py
@@ -4,6 +4,8 @@ TODO List:
 - Dense Interpolation
 - Richardson Extrapolation
 - Embedded Runge-Kutta Formula
+- Warning if using fixed timesteps and step does not exactly divide time span
+- Transfer error plot to jupyter
 """
 from __future__ import annotations
 
@@ -13,13 +15,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def euler_step(f: callable, t: float, y: np.ndarray, h: float) -> np.ndarray:
-    return y + h*f(t,y)
+    return y + h*np.asarray(f(t,y))
 
 def rk4_step(f: callable, t: float, y: np.ndarray, h: float) -> np.ndarray:
-    k1 = f(t,y)
-    k2 = f(t + h/2, y + h*k1/2)
-    k3 = f(t + h/2, y + h*k2/2)
-    k4 = f(t + h, y + h*k3)
+    k1 = np.asarray(f(t,y))
+    k2 = np.asarray(f(t + h/2, y + h*k1/2))
+    k3 = np.asarray(f(t + h/2, y + h*k2/2))
+    k4 = np.asarray(f(t + h, y + h*k3))
     
     return y + (1/6) * (k1 + 2*k2 + 2*k3 + k4)*h
 
@@ -39,9 +41,20 @@ def solve_to(f: callable, y0: np.ndarray, tspan: tuple[float, float], h: float, 
     return ODEResult(np.asarray(y), np.asarray(t))
 
 if __name__ == "__main__":
-    ode1 = lambda t, y: y
-    res = solve_to(ode1, [1], (0, 1), 1e-3, euler_step)
-    plt.plot(res.t, res.y)
-    res = solve_to(ode1, [1], (0, 1), 1e-3, rk4_step)
-    plt.plot(res.t, res.y)
+    ode = lambda t, y: [y[1], -y[0]]
+    res = solve_to(ode, [1, 0], (0, 10), 1e-3, rk4_step)
+    plt.plot(res.t, res.y[:, 0])
+    plt.show()
+    
+    x_true = lambda t : np.cos(t)
+    rk4_err = []
+    euler_err = []
+    hs = np.logspace(-1, -12, base=2)
+    for h in hs:
+        res = solve_to(ode, [1, 0], (0, 1), h, rk4_step)
+        rk4_err.append(np.abs(x_true(res.t[-1]) - res.y[-1,0]))
+        res = solve_to(ode, [1, 0], (0, 1), h, euler_step)
+        euler_err.append(np.abs(x_true(res.t[-1]) - res.y[-1,0]))
+    plt.loglog(hs, rk4_err)
+    plt.loglog(hs, euler_err)
     plt.show()
