fc9614e6119eaa57ef5e40257b1e5074aa236ef5
Jake Bowhay <jb9.bowhay@gmail.com>
2023-02-07 21:17:12 +0000

    move error plot to examples file

--- src/scicomp/integrate.py
+++ src/scicomp/integrate.py
@@ -32,20 +32,4 @@ def solve_to(f: callable, y0: np.ndarray, tspan: tuple[float, float], h: float, 
     return ODEResult(np.asarray(y), np.asarray(t))
 
 if __name__ == "__main__":
-    ode = lambda t, y: [y[1], -y[0]]
-    res = solve_to(ode, [1, 0], (0, 10), 1e-3, rk4_step)
-    plt.plot(res.t, res.y[:, 0])
-    plt.show()
-    
-    x_true = lambda t : np.cos(t)
-    rk4_err = []
-    euler_err = []
-    hs = np.logspace(-1, -12, base=2)
-    for h in hs:
-        res = solve_to(ode, [1, 0], (0, 1), h, rk4_step)
-        rk4_err.append(np.abs(x_true(res.t[-1]) - res.y[-1,0]))
-        res = solve_to(ode, [1, 0], (0, 1), h, euler_step)
-        euler_err.append(np.abs(x_true(res.t[-1]) - res.y[-1,0]))
-    plt.loglog(hs, rk4_err)
-    plt.loglog(hs, euler_err)
-    plt.show()
+    ...
