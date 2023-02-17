fdb5999b65e09e73759f07acb81a5781744f2fee
Jake Bowhay <jb9.bowhay@gmail.com>
2023-02-17 12:43:09 +0000

    add test for shooting

---
+++ test/test_shooting.py
@@ -0,0 +1,12 @@
+from scicomp.odes import hopf_normal
+from scicomp.shooting import find_limit_cycle#
+
+import numpy as np
+
+class TestFindLimitCycle:
+    def test_find_hopf_period(self):
+        beta = 1
+        rho = -1
+        np.testing.assert_allclose(
+            find_limit_cycle(lambda t, y: hopf_normal(t, y, beta, rho), [1, 1, 6])[-1],
+            2*np.pi, rtol=1e-4)
