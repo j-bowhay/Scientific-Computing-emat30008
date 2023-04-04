52dc0b6632802ee6fbdc817675210187453210f8
Jake Bowhay <jb9.bowhay@gmail.com>
2023-04-04 14:25:21 +0100

    add test with source term

--- test/test_bvps.py
+++ test/test_bvps.py
@@ -6,6 +6,7 @@ from scicomp.bvps import solve_linear_poisson_eq
 
 class TestSolveLinearPoissonEquation:
     def test_no_source(self):
+        D = 1
         a = 0
         b = 10
         gamma_1 = 2
@@ -16,7 +17,7 @@ class TestSolveLinearPoissonEquation:
         right_BC = DirichletBC(gamma_2)
 
         sol = solve_linear_poisson_eq(
-            grid=grid, left_BC=left_BC, right_BC=right_BC, D=1, q=lambda x: 0
+            grid=grid, left_BC=left_BC, right_BC=right_BC, D=D, q=lambda x: 0
         )
 
         assert sol.size == 100
@@ -27,4 +28,34 @@ class TestSolveLinearPoissonEquation:
 
         # compare to analytical solution
         expected = ((gamma_2 - gamma_1) / (b - a)) * (grid.x - a) + gamma_1
+        assert_allclose(sol, expected)
+
+    def test_source(self):
+        D = 15
+        a = 0
+        b = 10
+        gamma_1 = 2
+        gamma_2 = 30
+
+        grid = Grid(a=a, b=b, N=100)
+        left_BC = DirichletBC(gamma_1)
+        right_BC = DirichletBC(gamma_2)
+
+        sol = solve_linear_poisson_eq(
+            grid=grid, left_BC=left_BC, right_BC=right_BC, D=D, q=lambda x: 1
+        )
+
+        assert sol.size == 100
+
+        # check BCs
+        assert sol[0] == gamma_1
+        assert sol[-1] == gamma_2
+
+        # compare to analytical solution
+        x = grid.x
+        expected = (
+            (-1 / (2 * D)) * (x - a) * (x - b)
+            + ((gamma_2 - gamma_1) / (b - a)) * (grid.x - a)
+            + gamma_1
+        )
         assert_allclose(sol, expected)
