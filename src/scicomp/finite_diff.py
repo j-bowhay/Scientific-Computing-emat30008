2ba59d61b997a556fd7ea2114f6cbfde58b02f0d
Jake Bowhay <jb9.bowhay@gmail.com>
2023-04-04 10:34:03 +0100

    add grid class for finite differencing

--- src/scicomp/finite_diff.py
+++ src/scicomp/finite_diff.py
@@ -1,3 +1,5 @@
+from abc import ABC
+
 import numpy as np
 import scipy
 from scipy._lib._finite_differences import _central_diff_weights
@@ -36,3 +38,37 @@ def get_central_diff_matrix(
     A = scipy.sparse.diags(diags, offsets)
 
     return A if sparse else A.toarray()
+
+
+class Grid:
+    def __init__(self, a: float, b: float, N: int) -> None:
+        """Grid class for representing the domain in finite difference methods
+
+        Parameters
+        ----------
+        a : float
+            Position of the left side of the domain
+        b : float
+            Position of the right side of the domain
+        N : int
+            Number of points to divide the domain into
+        """
+        if b <= a:
+            raise ValueError("'b' must be greater than 'a'")
+        if N < 1:
+            raise ValueError("'N' must be positive")
+
+        self.a = a
+        self.b = b
+        self.x = np.linspace(a, b, N)
+
+    @property
+    def x_inner(self) -> np.ndarray:
+        """Get the inner points of the domain
+
+        Returns
+        -------
+        np.ndarray
+            Location of the inner elements of the grid
+        """
+        return self.x[1:-1]
